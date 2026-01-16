de8e4b9e6e86aad824439027a125ecdf87cd33fb
0-Umer-0 <azizakmal575@gmail.com>
2026-01-16 19:58:35 +0500

    class2

---
+++ main.js
@@ -0,0 +1,53 @@
+// const express = require("express")
+
+// const app = express()
+
+
+// const data = {
+//     name: "umer akmal",
+//     age: 18,
+
+//     occupation: "Software Developer"
+// }
+
+// app.get("/ali",(req,res)=>{
+//     res.json(data)
+// })
+
+// app.listen(8000,()=>{
+//     console.log("Server is running on port 8000")
+// })
+
+
+
+// const express = require ("express")
+// const app = express()
+
+// let data = {
+//     name: "Muhammed Al akmal",
+//     age: 16,
+//     occupation: "Software Developer"
+// }
+
+// const user = {
+//     name: "Ali Akbar",
+//     age: 20,
+//     occupation: "Web Developer"
+// }
+
+
+// app.get("/",(req,res )=> {
+//     res.json(data)
+// })
+
+// app.get("/user",(req,res)=>{
+//     res.json(user)
+// })
+
+// app.listen(8000,()=> {
+//     console.log("server 8000 is running");
+    
+// })
+
+// //----------------------------------------------------------------------------------------------------------------//
+
